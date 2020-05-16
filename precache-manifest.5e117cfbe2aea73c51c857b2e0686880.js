6293665f8f92eda2e8d948e1838ae0b604aae2b6
conorburke <cjburke89@gmail.com>
2020-05-16 13:54:45 +0900

    update to latest build

---
+++ precache-manifest.5e117cfbe2aea73c51c857b2e0686880.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0470e177c9de817acecd21bf99627452",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8acd6ec5d8ccfe1cb4e4",
+    "url": "/static/css/main.be8c760a.chunk.css"
+  },
+  {
+    "revision": "1f42baac4da7d48efe12",
+    "url": "/static/js/2.55a4190f.chunk.js"
+  },
+  {
+    "revision": "8acd6ec5d8ccfe1cb4e4",
+    "url": "/static/js/main.984a0d4c.chunk.js"
+  },
+  {
+    "revision": "19c9763f4eeb5b81a01d",
+    "url": "/static/js/runtime-main.69192133.js"
+  },
+  {
+    "revision": "21608779b67f4d52e1f21f6cd54d9d75",
+    "url": "/static/media/claire.21608779.jpg"
+  },
+  {
+    "revision": "f97af1842c5cbf96eda180e24d999bc4",
+    "url": "/static/media/magnetar_tools_page.f97af184.png"
+  },
+  {
+    "revision": "98c3c009783aae5b5f699f95ce6ce3b7",
+    "url": "/static/media/mancala.98c3c009.png"
+  },
+  {
+    "revision": "e67f8a71732b9c52d337abee348f6d97",
+    "url": "/static/media/seespotgo.e67f8a71.png"
+  },
+  {
+    "revision": "9c04c12bfa38592058bb22903b71d8a6",
+    "url": "/static/media/telescopes.9c04c12b.png"
+  },
+  {
+    "revision": "4230a9ca5027ba6a8dd1174c54232a3b",
+    "url": "/static/media/walter.4230a9ca.png"
+  },
+  {
+    "revision": "2dff5d619c9404b8d58bd0b665b40f82",
+    "url": "/static/media/yoda.2dff5d61.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e37012b0975c417e127f248b04129593.js"
+  "/precache-manifest.5e117cfbe2aea73c51c857b2e0686880.js"
 );
 
 self.addEventListener('message', (event) => {
